cb62d851aa9c93fa1ad94eba1c571624f0f164bd
XavierChanth <xchanthavong@gmail.com>
2020-02-05 11:32:49 -0500

    add middleware for the app

--- endpoints/index.js
+++ endpoints/index.js
@@ -15,6 +15,11 @@ export default (app) => {
 
   });
 
+  // Middleware
+  app.use((err, req, res, next) => {
+    // runs when calling next [add to (req, res) args to become (req, res, next)]
+  });
+
   app = initApi(app);
   return app;
 };
